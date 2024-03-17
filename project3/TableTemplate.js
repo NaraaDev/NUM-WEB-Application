3d7220a4512d60f0623db7e3375c7cc926b507b6
Naranbat Batsaikhan <99318619+NaraaDev@users.noreply.github.com>
2024-03-17 16:12:43 +0000

    feat: asdfaasdf

---
+++ project3/TableTemplate.js
@@ -0,0 +1,27 @@
+"use strict";
+class TableTemplate {
+  static fillIn(id, dict, columnName) {
+    const table = document.getElementById(id);
+    table.style.visibility = "visible";
+    if (!columnName) {
+      const fill = new Cs142TemplateProcessor(table.innerHTML);
+      table.innerHTML = fill.fillIn(dict);
+    } else if (columnName === "Part Number") {
+      let row = table.firstElementChild.firstElementChild;
+      while (row) {
+        const c = row.firstElementChild;
+        const fill = new Cs142TemplateProcessor(c.textContent);
+        c.textContent = fill.fillIn(dict);
+        row = row.nextElementSibling;
+      }
+    } else if (columnName === "Length") {
+      let row = table.firstElementChild.firstElementChild;
+      while (row) {
+        const c = row.lastElementChild;
+        const fill = new Cs142TemplateProcessor(c.textContent);
+        c.textContent = fill.fillIn(dict);
+        row = row.nextElementSibling;
+      }
+    }
+  }
+}
